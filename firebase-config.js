4b94b7184faa4c4f4a5c238c7d0aee4241ea6b66
joshuewok674 <joshuewok674@outlook.com>
2026-04-01 04:46:07 +0000

    Making sure everything works pt. 2

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,27 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { 
+    getFirestore, 
+    collection, 
+    addDoc, 
+    onSnapshot, 
+    query, 
+    where, 
+    orderBy, 
+    updateDoc, 
+    doc,
+    deleteDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBbPEHhZb1dASOjgZAbUP_g1AbI6wmZCzw",
   authDomain: "jy-3d-design-studio.firebaseapp.com",
   projectId: "jy-3d-design-studio",
-  storageBucket: "jy-3d-design-studio.firebasestorage.app",
+  storageBucket: "jy-3d-design-studio.appspot.com",
   messagingSenderId: "307590724871",
   appId: "1:307590724871:web:770275eb0beacb6c4899a2",
   measurementId: "G-TSN1PQRNMT"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
