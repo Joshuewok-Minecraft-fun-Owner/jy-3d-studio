594408ab1ea3b2d8f41b44c13c8330ae0385c288
Joshua Yang <166943317+Joshuewok-Minecraft-fun-Owner@users.noreply.github.com>
2026-06-07 17:35:57 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -14,15 +14,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBbPEHhZb1dASOjgZAbUP_g1AbI6wmZCzw",
-  authDomain: "jy-3d-design-studio.firebaseapp.com",
-  projectId: "jy-3d-design-studio",
-  storageBucket: "jy-3d-design-studio.firebasestorage.app",
-  messagingSenderId: "307590724871",
-  appId: "1:307590724871:web:770275eb0beacb6c4899a2",
-  measurementId: "G-TSN1PQRNMT"
-};
+const firebaseConfig = ${{ secrets.FIREBASE_CONFIG }};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
